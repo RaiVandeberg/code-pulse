5cbd9b79b280f1eaf9c0421232ac862edfb35e18
Raí Vandenberg <rvdesigner20@gmail.com>
2025-07-14 21:21:20 -0300

    feat: add CompletedLesson model and markLessonAsCompleted action for tracking completed lessons

---
+++ src/actions/user.ts
@@ -0,0 +1,29 @@
+"use server"
+
+import { prisma } from "@/lib/prisma"
+import { auth } from "@clerk/nextjs/server"
+
+export const getUser = async () => {
+
+    const { userId } = await auth()
+
+    if (!userId) {
+        throw new Error("User not authenticated")
+    }
+
+    const user = await prisma.user.findUnique({
+        where: {
+            clerkUserId: userId
+        }
+    })
+
+    if (!user) {
+        throw new Error("User not found")
+    }
+
+    return {
+        user,
+        clerkUserId: userId,
+        userId: user.id
+    }
+}
---
+++ src/lib/tanstack-query.ts
@@ -0,0 +1,5 @@
+
+
+export const queryClient = new QueryClient({
+
+})
